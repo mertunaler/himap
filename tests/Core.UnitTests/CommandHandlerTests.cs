b1e74f8f6097822bd7ac91878e12ecc4cbaad816
mertunaler <mertunaler@gmail.com>
2024-08-13 17:42:06 +0300

    test: tag tests added

--- tests/Core.UnitTests/CommandHandlerTests.cs
+++ tests/Core.UnitTests/CommandHandlerTests.cs
@@ -7,7 +7,7 @@ public class UnitTest1
     [Fact]
     public void GeneratesValidCommand()
     {
-        string expectedCmd = "LOGIN w3rt w3rt";
+        string expectedCmd = "A0000 LOGIN w3rt w3rt";
         var sut = GetCommandBuilderObject();
 
         string generatedCmd = sut.WithCommand("LOGIN")
@@ -19,13 +19,44 @@ public class UnitTest1
     [Fact]
     public void ReturnsValidCommandWithoutParams()
     {
-        string expectedCmd = "LOGIN";
+        string expectedCmd = "A0000 LOGIN";
         var sut = GetCommandBuilderObject();
 
         string generatedCmd = sut.WithCommand("LOGIN")
                                   .Build();
 
         Assert.Equal(expectedCmd, generatedCmd);
+    }
+    [Fact]
+    public void CommandGetCorrectTag()
+    {
+        var sut = GetCommandBuilderObject();
+        string expectedTag = "A0000";
+        string generatedCmd = sut.WithCommand("LOGIN")
+                             .WithParameters("w3rt")
+                             .Build();
+
+        string generatedTag = generatedCmd.Split(" ")[0];
+
+        Assert.Equal(expectedTag, generatedTag);
+    }
+    [Fact]
+    public void MultipleCommandsGeneratesValidCounteredCommands()
+    {
+        var sut = GetCommandBuilderObject();
+
+        string firstCmd = sut.WithCommand("LOGIN")
+                             .WithParameters("w3rt")
+                             .Build();
+
+        string secondCmd = sut.WithCommand("LOGIN")
+                             .WithParameters("drogba")
+                             .Build();
+
+        string firstTag = firstCmd.Split(" ")[0];
+        string secondTag = secondCmd.Split(" ")[0];
+
+        Assert.NotEqual(firstTag, secondTag);
     }
     private IMAPCommandBuilder GetCommandBuilderObject()
     {
