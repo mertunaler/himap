a054c323f98b7378ac572497ea0747cad8ccdb35
mertunaler <mertunaler@gmail.com>
2024-08-16 18:53:46 +0300

    fix: cors adjustments for frontend

--- IMAP.API/Program.cs
+++ IMAP.API/Program.cs
@@ -1,11 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Frontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")  
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -15,8 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
+app.UseCors("Frontend");
 
 app.UseAuthorization();
 
