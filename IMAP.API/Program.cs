761bc07bad34ac5c738e0b9a79da5a2f20eac78a
mertunaler <mertunaler@gmail.com>
2024-08-17 20:27:14 +0300

    fix: cors origin change

--- IMAP.API/Program.cs
+++ IMAP.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")  
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
